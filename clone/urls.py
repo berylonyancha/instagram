9ffa5b0efe6c3690b8f3b910c3f85bef187ed139
Beryl Onyancha <berylonyancha@gmail>
2019-07-29 12:58:19 +0300

    Add post url

--- clone/urls.py
+++ clone/urls.py
@@ -4,5 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-  url('^$',views.homepage,name='homePage')
+  url('^$',views.homepage,name='homePage'),
+  url(r'^post',views.post,name='post')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
