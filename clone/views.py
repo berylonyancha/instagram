bb2141870a9065844362afbed788baddd87411f7
Beryl Onyancha <berylonyancha@gmail>
2019-07-29 12:57:50 +0300

    Create function post to handle new posts

--- clone/views.py
+++ clone/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+import datetime as dt
+from django.contrib.auth.decorators import login_required
+from .models import Image, Profile,Comments
+from .forms import NewComment,NewImage,UpdateProfile
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='/accounts/login')
+def homepage (request):
+    return render(request,'home.html')
+
+@login_required(login_url='/accounts/login')
+def post(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewImage(request.POST,request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.user = current_user
+            image.save()
+        return redirect('homepage')
+    else:
+        form = NewImage()
+    return render(request, 'post.html', {"form": form})
+
