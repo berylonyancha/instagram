aa60f698e176dcda339706a799ba30b43a846762
Beryl Onyancha <berylonyancha@gmail>
2019-07-29 09:42:37 +0300

    Add Image model and create methods

--- clone/models.py
+++ clone/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 # Create your models here.
+class Image(models.Model):
+      image = models.ImageField(upload_to='images/')
+      name = models.CharField(max_length =30)
+      image_caption = HTMLField()
+      post_date = models.DateTimeField(auto_now_add =True)
+      user=models.ForeignKey(User)
+      likes=models.IntegerField(default=0)
+
+      def __str__(self):
+          return self.name
+      
+      class Meta:
+          ordering =['-post_date']
+      
+      def save_image(self):
+          self.save()
+
+      def delete_image(self):
+          self.delete()
+
+      @classmethod
+      def all_images(cls):
+          images = cls.objects.all()
+          return images
+      @classmethod
+      def search_by_users(cls,term):
+          result=cls.objects.filter(user__username__icontains=term)
+          return result
+
+
