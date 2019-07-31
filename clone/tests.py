937c85f7c8ede0ceebfc3f3bf8e0dca9a1dca3ac
Beryl Onyancha <berylonyancha@gmail>
2019-07-31 15:16:31 +0300

    create tests for Profile

--- clone/tests.py
+++ clone/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+import datetime as dt
+from .models import Profile,Image,Comment
+from django.contrib.auth.models import User
+
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.Beryl= Profile(profile_photo = 'Cats',bio ='in the rari with no tint', user_id = 1)
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Beryl,Profile))
+
+    def test_save_method(self):
+        self.Beryl.save_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile) > 0)
+
+    def test_delete_method(self):
+        self.John.delete_profile
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) > 0)
+
+
+
