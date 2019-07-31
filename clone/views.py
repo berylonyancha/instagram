88b61e0e7d8f0cbc9bbcf4604219cbae36061e24
Beryl Onyancha <berylonyancha@gmail>
2019-07-31 15:18:58 +0300

    Create view function for new profile

--- clone/views.py
+++ clone/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render,redirect
 import datetime as dt
 from django.contrib.auth.decorators import login_required
-from .models import Image, Profile,Comments
+from .models import Image, Profile,Comments,Likes
 from .forms import NewComment,NewImage,UpdateProfile
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.http import Http404,HttpResponse,HttpResponseRedirect
+
 
 # Create your views here.
 @login_required(login_url='/accounts/login')
 def homepage (request):
-    return render(request,'home.html')
+    current_user = request.user
+    all_images = Image.objects.all()
+    comments = Comments.objects.all()
+    likes = Likes.objects.all()
+    profile = Profile.objects.all()
+    print(likes)
+    return render(request,'home.html',locals())
 
 @login_required(login_url='/accounts/login')
 def post(request):
@@ -19,8 +27,22 @@ def post(request):
             image = form.save(commit=False)
             image.user = current_user
             image.save()
-        return redirect('homepage')
+        return redirect('homePage')
     else:
         form = NewImage()
-    return render(request, 'post.html', {"form": form})
+    return render(request, 'post.html',locals())
+
+
+@login_required(login_url='/accounts/login')
+def profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = UpdateProfile(request.POST,request.FILES)
+        if form.is_valid():
+             profile = form.save(commit=False)
+             profile.user = current_user
+             profile.save()
+        else:
+            form=UpdateProfile()
+    return render(request, 'profile/new.html',locals())
 
