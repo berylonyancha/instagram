31c42f4f6d2dbf09fa8fcd3853057e7eef22516d
Beryl Onyancha <berylonyancha@gmail>
2019-07-31 15:17:18 +0300

    create tests for Image and Comment models

--- clone/tests.py
+++ clone/tests.py
@@ -22,4 +22,52 @@ class ProfileTestClass(TestCase):
         self.assertTrue(len(profiles) > 0)
 
 
+class ImageTestClass(TestCase):
+    def setUp(self):
+        self.Cats= Image(photo='cats.jpg',image_name='rio',image_caption='cute cats',post_date='2019-07-30 11:30:33.999607+03',likes='t',profile =User.id)
+    def test_instance(self):
+        self.assertTrue(isinstance(self.cats,Image))
+
+    def test_save_method(self):
+        self.Cats.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
+
+    def test_delete_method(self):
+        self.Cats.delete_image
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
+
+def tearDown(self):
+       Profile.objects.all().delete()
+       Comment.objects.all().delete()
+       Image.objects.all().delete()
+class CommentTestClass(TestCase):
+   def setUp(self):
+     
+        self.new_user = User.objects.create_user("testuser", "secret")
+
+        self.new_profile = Profile(profile_pic='cats.jpg',bio="in the rari with no tint",owner=self.new_user)
+        self.new_profile.save()
+
+        self.new_image = Image(pic='cats.jpg',caption="this is a test image", profile='',profile_details=self.new_user)
+        self.new_image.save()
+
+        self.new_comment = Comment(
+            image=self.new_image, comment_owner=self.new_profile, comment="this is a test comment on a post")
+
+    def test_instance_true(self):
+        self.new_comment.save()
+        self.assertTrue(isinstance(self.new_comment, Comment))
+
+    def test_save_comment(self):
+        self.new_comment.save_comment()
+        comments = Comment.objects.all()
+        self.assertTrue(len(comments) == 1)
+
+    def tearDown(self):
+        Image.objects.all().delete()
+        Profile.objects.all().delete()
+        User.objects.all().delete()
+        Comment.objects.all().delete()
 
