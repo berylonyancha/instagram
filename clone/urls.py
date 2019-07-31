66a413152fc974d75b1aefce55c46bf36031ef39
Beryl Onyancha <berylonyancha@gmail>
2019-07-31 15:21:11 +0300

    Add urls links to show profile, search and comment

--- clone/urls.py
+++ clone/urls.py
@@ -5,7 +5,11 @@ from django.conf.urls.static import static
 
 urlpatterns=[
   url('^$',views.homepage,name='homePage'),
-  url(r'^post',views.post,name='post')
-]
+  url(r'^post',views.post,name='post'),
+  url(r'^profile$',views.profile,name='profile'),
+  url(r'^user/(?P<username>\w+)',views.showprofile,name ='showprofile'),
+  url(r'^ search/',views.search, name='search'),
+  url(r'^comment/(?P<image_id>\d+)', views.comment, name='comment'),
+  
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
